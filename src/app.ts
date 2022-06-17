a65b9846e064defbd0451720012908fd8c6a0995
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-17 18:23:26 +0700

    another generic function

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 // Code goes here!
 
-//Create a function that will
-function merge<T, U>(objA: T, objB: U) {
+//Use constraints
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
 
-const mergeObj = merge({name: 'Bao'}, {age:26});
+const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Bao', hobbies: ['play','game']}, {age:26});
 console.log(mergeObj.name)
-console.log(mergeObj.age)
+console.log(mergeObj.hobbies)
+
+
+// ============= another generic function =============================
+interface Lengthy {
+    length: number;
+  }
+  
+  function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+      descriptionText = 'Got 1 element.';
+    } else if (element.length > 1) {
+      descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+  }
+  
+  console.log(countAndDescribe(['Sports', 'Cooking']));
+//   console.log(countAndDescribe(10));
